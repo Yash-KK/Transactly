57851aa2831c5e002ab33e928e37a68318f00140
Yash-KK <reachyashk@gmail.com>
2025-01-31 22:37:59 +0530

    add sidebar-navigation

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Sidebar from "@repo/ui/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main>{children}</main>
+      <main className="flex">
+        <Sidebar />
+        <div className="ml-64">
+        {children}
+        </div>
+        
+      </main>
     </>
   );
 }
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,30 +1,9 @@
-"use client";
-import { useSession } from "next-auth/react";
-import React from "react";
-export default function Page() {
-  const session = useSession();
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 
-  return (
-    <>
-      <div className="flex justify-center items-center h-[calc(100vh-64px)] bg-gray-900">
-        <a
-          href="#"
-          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100"
-        >
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-            {session.status === "authenticated"
-              ? "You are authenticated"
-              : "You are not authenticated"}
-          </h5>
-          {session.status === "authenticated" && (
-            <>
-              <p className="font-normal text-gray-700">
-                Email: {session.data.user?.email}
-              </p>
-            </>
-          )}
-        </a>
-      </div>
-    </>
-  );
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session === null) redirect("/signin");
+  else redirect("/dashboard");
 }
